d31442e99c2739dccb74d47d34352e75bdadb639
jaswindersingh2 <jaswinder.singh3@griffithuni.edu.au>
2020-03-10 16:58:47 +1000

    added path for nt database

--- run_rnasnap2.sh
+++ run_rnasnap2.sh
@@ -4,10 +4,10 @@ input=$1
 input_dir=$(dirname $1)
 seq_id=$(basename $(basename $input) .fasta)
 
-path_blastn=./ncbi-blast-2.10.0+/bin/blastn
-db_blastn=$2 #/mnt/hdd6/jaswinder/Documents/blastn_db/nt
+path_blastn=./ncbi-blast-2.10.0+/bin/
+path_blastn_database=./nt_database/nt
 path_infernal=./infernal-1.1.3-linux-intel-gcc/binaries
-db_inf=$3   #/mnt/hdd6/jaswinder/Documents/infernal/nt_database/nt
+path_infernal_database=./nt_database/nt
 
 #echo $input_dir/$seq_id.bla
 
@@ -20,7 +20,7 @@ else
 	if [ -f $input_dir/$seq_id.aln ];	then
 		echo "blastn output exists";
 	else
-		$path_blastn/blastn -db $db_blastn -query $input -out $input_dir/$seq_id.bla -evalue 0.001 -num_descriptions 1 -num_threads 8 -line_length 1000 -num_alignments 50000
+		$path_blastn/blastn -db $path_blastn_database -query $input -out $input_dir/$seq_id.bla -evalue 0.001 -num_descriptions 1 -num_threads 8 -line_length 1000 -num_alignments 50000
 		./parse_blastn_local.pl $input_dir/$seq_id.bla $input_dir/$seq_id.fasta $input_dir/$seq_id.aln
 	fi
 
@@ -28,7 +28,7 @@ else
 	./reformat.pl fas sto $input_dir/$seq_id.aln $input_dir/$seq_id.sto
 	$path_infernal/cmbuild --noss --hand -F $input_dir/$seq_id.cm $input_dir/$seq_id.sto
 	#$path_infernal/cmcalibrate $input_dir/$seq_id.cm
-	timeout 7200s $path_infernal/cmsearch -o $input_dir/$seq_id.out --cpu 30 --incE 10.0 $input_dir/$seq_id.cm $db_inf
+	timeout 7200s $path_infernal/cmsearch -o $input_dir/$seq_id.out --cpu 30 --incE 10.0 $input_dir/$seq_id.cm $path_infernal_database
 
 	######### get pssm from either from infernal and blstn alignment #############
 	if [ -s $input_dir/$seq_id.out ];	then
